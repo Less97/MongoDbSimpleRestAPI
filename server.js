97c5c7a26dcfc1f0ec1dbd7c84356b8699354105
Less97 <alessandro83lignano@gmail.com>
2016-10-12 17:51:29 +0100

    [Working] - Working on the integration with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+mongoose.connect('mongodb://localhost:4321/myVegAppDb');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	username: {type:String,required:true},
+	password: {type:String,required:true},
+	tipology:{type:String,required:true},
+	firstName:{type:String,required:true},
+	lastName:{type:String,required:true},
+	email:{type:String,required:true},
+});
+
+var User = mongoose.model('User',userSchema)
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  User.find({},function(err,users){})
+	if(err) throw error;
+	return users.toString();
+	
 });
 
 app.listen(3000, function () {
